cc33551ef2479f868db4969cefbc7e87ac9c7aba
eluve <eluvemail@gmail.com>
2026-08-27 05:39:04 +0200

    Revertir los cambios del correo por api raro ded la IA

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'no-responder@trackbitos.host';
-    public string $fromName   = 'Trackbitos';
+    public string $fromEmail  = '';
+    public string $fromName   = '';
     public string $recipients = '';
 
     /**
